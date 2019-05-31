037ffad9fb03f71e2a7a7e4f94abb533a9d1eb17
Cristhian Murcia <cristhian.murcia@globant.com>
2019-05-30 22:04:36 -0500

    Modified embeddings script and added explanation images

--- 2.face_recognition/get_embeddings.py
+++ 2.face_recognition/get_embeddings.py
@@ -4,12 +4,13 @@ import cv2
 import face_recognition
 import pickle
 
-
-
-
 # path pointing to the project directory
 base_dir = os.path.dirname(os.path.abspath("__file__"))
-output_embeddings = os.path.sep.join([base_dir, "models"])
+output_embeddings = os.path.sep.join([base_dir, "model"])
+
+if not os.path.exists(output_embeddings):
+    os.makedirs(output_embeddings)
+
 # Load Images
 metadata = helper_functions.load_metadata('images')
 
@@ -28,28 +29,32 @@ for (i, imagePath) in enumerate(imagePaths):
 
     # load the input image and convert it from RGB (OpenCV ordering)
     # to dlib ordering (RGB)
-    image = cv2.imread(imagePath)
-    image = helper_functions.resize_image(image, 600)
-    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    try :
+        image = cv2.imread(imagePath)
+        image = helper_functions.resize_image(image, 600)
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # detect the (x, y)-coordinates of the bounding boxes
-    # corresponding to each face in the input image
-    boxes = face_recognition.face_locations(rgb,
-                                            model="hog")
+        # detect the (x, y)-coordinates of the bounding boxes
+        # corresponding to each face in the input image
+        boxes = face_recognition.face_locations(rgb,
+                                                model="hog")
 
-    # compute the facial embedding for the face
-    encodings = face_recognition.face_encodings(rgb, boxes)
+        # compute the facial embedding for the face
+        encodings = face_recognition.face_encodings(rgb, boxes)
 
-    # loop over the encodings
-    for encoding in encodings:
-        # add each encoding + name to our set of known names and
-        # encodings
-        knownEncodings.append(encoding)
-        knownNames.append(name)
+        # loop over the encodings
+        for encoding in encodings:
+            # add each encoding + name to our set of known names and
+            # encodings
+            knownEncodings.append(encoding)
+            knownNames.append(name)
+    except:
+        continue
 
 # dump the facial encodings + names to disk
 print("[INFO] serializing encodings...")
 data = {"embeddings": knownEncodings, "names": knownNames}
-f = open(os.path.sep.join(["model", "embedding.pickle"]), "wb")
+path = os.path.sep.join(["model", "embedding.pickle"])
+f = open(path, "wb")
 f.write(pickle.dumps(data))
 f.close()
